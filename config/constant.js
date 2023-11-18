c3b67bd27a4815f1503eaccdb04732dce3cf443b
Thanh Nguyen <nvthanh.hcm>
2023-11-18 16:05:47 +0700

    cm.

--- config/constant.js
+++ config/constant.js
@@ -8,7 +8,7 @@ import VueAxios from 'vue-axios'
 Vue.use(VueAxios, axios)
 
 // Set the base URL for your API
-Vue.axios.defaults.baseURL = 'http://localhost:8000/'
-
+// Vue.axios.defaults.baseURL = 'http://localhost:8000/'
+Vue.axios.defaults.baseURL = 'https://py-emp-83acf99169e4.herokuapp.com/'
 // Export the configured instance
 export default Vue.axios
